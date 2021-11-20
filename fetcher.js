39574a4a2d0d559afab2e942b1128194886dcbf0
Brian Duong <brianminhduong@gmail.com>
2021-11-20 07:21:25 +0000

    Add check for valid specified location and URL error check

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,20 @@
 const request = require('request');
 const fs = require('fs');
+const { exit } = require('process');
 const urlInputs = process.argv.slice(2);
 const domainLink = urlInputs[0];
 const specifiedPath = urlInputs[1];
 
+fs.access(specifiedPath, (err) =>{
+  if (err) throw err;
+});
+
 request(domainLink, (error, response, body) => {
+  if (error) {
+    console.log(`Error due to an URL error or non-200`);
+    exit();
+  }
+
   fs.writeFile(specifiedPath, body, function(err) {
     if (err) throw err;
   });
